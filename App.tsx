0ea07e06f2a22757f649098258904b439a1764bc
Maxence Rossignol <maxence.rossignol@renewex.co>
2025-04-20 08:59:50 +0200

    upd(first basic visual of front using tailwind)

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation';
+import "./global.css"
 
 export default function App() {
     return (

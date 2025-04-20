91ac92fe5c3f5b820eb30d2526f1f4e40c34224b
Maxence Rossignol <maxence.rossignol@renewex.co>
2025-04-20 09:22:27 +0200

    fix(upd dependencies)

--- src/components/TailwindTest.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export default function TailwindTest() {
-  return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-2xl font-bold">Tailwind is working 🎉</Text>
-    </View>
-  )
-}
